750f18b796c03ce3a56cc44e9c6ab01bd20adb3b
jenybo <86452879+JenyBo@users.noreply.github.com>
2022-10-25 21:10:27 +0700

    week1_commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/86/b0ba36776c54001d1371d54b3543b2e9
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ADMIN
+ *
+ */
+module OOP_Learning {
+}
---
+++ OOP_Learning_week1/src/OOP/Test.java
@@ -0,0 +1,8 @@
+package OOP;
+
+public class Test {
+	public static void main(String args[]) {
+		System.out.println("Xin chao \n cac ban!");
+		System.out.println("Heloo \t world!");
+	}
+}
