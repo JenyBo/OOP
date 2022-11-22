0c562a4890f7654e13126e342eb78bb8f2b2c71e
jenybo <86452879+JenyBo@users.noreply.github.com>
2022-11-22 16:57:16 +0700

    test_overload

--- week2/src/week2/Cart.java
+++ week2/src/week2/Cart.java
@@ -4,6 +4,12 @@ public class Cart {
 	public static final int MAX_NUMBER_ORDERED = 20;
 	private int qtyOrdered = 0;
 	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+	public void addDigitalVideoDisc(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2) {
+		
+	}
+    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList) {
+		
+	}
 	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
 		if(qtyOrdered < MAX_NUMBER_ORDERED)
 		{
